75a2b0bab0934a61915dde70ea6a6c701f5af726
Walker Malling <walker.malling@gmail.com>
2017-07-07 16:40:20 -0700

    add logger and db wrapper

---
+++ log.js
@@ -0,0 +1,52 @@
+const ramda = require('ramda');
+const chalk = require('chalk');
+
+const compose = ramda.compose;
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const ensureType = (payload) => {
+  if (Array.isArray(payload)) {
+    return { data: payload };
+  }
+  if (typeof payload === 'object') {
+    return payload;
+  }
+  return { messsage: payload };
+};
+
+const addTime = (obj) => {
+  if (obj && obj.time) {
+    return obj;
+  }
+  return Object.assign({ time: Date.now() }, obj);
+};
+
+const format = (obj) => {
+  if (NODE_ENV === 'production') {
+    return JSON.stringify(obj);
+  }
+  return JSON.stringify(obj, null, 2);
+};
+
+const colorize = color => str => {
+  if (NODE_ENV === 'production') {
+    return str;
+  }
+  return chalk[color](str);
+};
+
+const log = color => compose(
+  console.log, // eslint-disable-line no-console
+  colorize(color),
+  format,
+  addTime,
+  ensureType
+);
+
+module.exports = {
+  error: log('red'),
+  warn: log('orange'),
+  info: log('white'),
+  verbose: log('gray'),
+};
---
+++ postgres.js
@@ -0,0 +1,23 @@
+const pg = require('pg');
+const log = require('./log');
+
+const config = {
+  max: 10,
+  idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+pool.on('error', (err, client) => {
+  log.error('client error', err.message, err.stack, client);
+});
+
+const query = (queryText, paramArray, callback) => {
+  pool.query(queryText, paramArray, callback);
+};
+
+const connect = (callback) => {
+  pool.connect(callback);
+};
+
+module.exports = { query, connect };
