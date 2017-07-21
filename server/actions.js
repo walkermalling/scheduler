69861be02f84674800aabc0081d69d9f9236def3
Walker Malling <walker.malling@gmail.com>
2017-07-21 11:15:38 -0700

    remove unused actions

--- server/actions.js
+++ server/actions.js
@@ -5,10 +5,6 @@ const queryBuilder = require('./query-builder');
 const validateQuery = require('./query-validator').validateQuery;
 const log = require('./log');
 
-const compose = ramda.compose;
-const head = ramda.head;
-const pathOr = ramda.pathOr;
-
 const Future = Fantasy.Future;
 
 // query :: Object -> Future
@@ -30,34 +26,4 @@ const query = (action, table, values, where) => Future((reject, resolve) => {
   });
 });
 
-// schedule :: Integer -> Future
-// given a Pioneer Id, lookup the coach, and get the coache's schedule
-const getSchedule = pioneerId => Future((reject, resolve) => {
-  const queryString = `SELECT c.id as id, c.pioneer_id, c.coach_id, c.time_start
-                 FROM calls as c
-                 WHERE c.coach_id = (SELECT coach_id 
-                                     FROM pioneers
-                                     WHERE id = ${pioneerId}
-                                    )`;
-  db.query(queryString, [], (error, results) => {
-    if (error) {
-      reject(error);
-    } else {
-      resolve(results);
-    }
-  });
-});
-
-// :: Integer -> Integer -> Object -> Future
-const validateCoach = (pioneerId, coachId, result) => Future((reject, resolve) => {
-  const pioneerRecord = compose(head, pathOr([], ['rows']))(result);
-  const coachIsMatch = pioneerRecord.id === pioneerId && pioneerRecord.coach_id === coachId;
-
-  if (coachIsMatch) {
-    resolve(true);
-  } else {
-    reject(false);
-  }
-});
-
-module.exports = { query, getSchedule, validateCoach };
+module.exports = { query };
